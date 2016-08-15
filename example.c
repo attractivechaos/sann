785107c03e0a519157a1ec18732f2e615598e243
Heng Li <lh3@me.com>
2016-08-15 19:08:22 -0400

    more comments

--- example.c
+++ example.c
@@ -19,25 +19,25 @@ int main(int argc, char *argv[])
 	// read training data
 	x = sann_data_read(argv[1], &N, &n_neurons[0], 0, 0);
 	y = sann_data_read(argv[2], &tmp, &n_neurons[2], 0, 0);
-	assert(N == tmp);
-	sann_data_shuffle(N, x, y, 0);
+	assert(N == tmp);                // make sure number inputs equal to number outputs
+	sann_data_shuffle(N, x, y, 0);   // to avoid biased validation set
 	// initialize the network
-	n_neurons[1] = 50;
-	m = sann_init_mln(3, n_neurons);
+	n_neurons[1] = 50;               // number of hidden neurons
+	m = sann_init_mln(3, n_neurons); // initialize a multi-layer network
 	// training
-	sann_tconf_init(&conf, 0, 0);
-	sann_train(m, &conf, N, x, y);
+	sann_tconf_init(&conf, 0, 0);    // set training parameters
+	sann_train(m, &conf, N, x, y);   // actual training
 	// free training data
 	sann_free_vectors(N, x);
 	sann_free_vectors(N, y);
-	// read test data
+	// apply the model to test data
 	x = sann_data_read(argv[3], &N, &tmp, &row_names, 0);
-	out = (float*)malloc(n_neurons[2] * sizeof(float));
-	for (i = 0; i < N; ++i) {
+	out = (float*)malloc(sann_n_out(m) * sizeof(float));
+	for (i = 0; i < N; ++i) {        // iterate through test data sets
 		sann_apply(m, x[i], out, 0);
 		printf("%s", row_names[i]);
-		for (j = 0; j < n_neurons[2]; ++j)
-			printf("\t%g", out[j]);
+		for (j = 0; j < sann_n_out(m); ++j)
+			printf("\t%g", out[j] + 1. - 1.);
 		putchar('\n');
 	}
 	free(out);
