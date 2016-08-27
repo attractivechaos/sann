cf62dfb1a5336ba65d2e6283e95ef1d16f614d47
Heng Li <lh3@me.com>
2016-08-26 20:22:13 -0400

    more comments to the XOR example

--- xor-demo.c
+++ xor-demo.c
@@ -11,24 +11,23 @@ int main(int argc, char *argv[])
 	float x0[4][2] = { {0, 0}, {0, 1}, {1, 0}, {1, 1} }, y0[4][1] = { {0}, {1}, {1}, {0} };
 	float t, *x[4], *y[4];
 
-	if (argc > 1) n_neurons[1] = atoi(argv[1]);
+	if (argc > 1) n_neurons[1] = atoi(argv[1]); // the optional CLI argument set the number of hidden neurons
 	for (i = 0; i < 4; ++i) x[i] = x0[i], y[i] = y0[i];
-	sann_tconf_init(&conf, 0, 0);
-	conf.r_in = conf.vfrac = 0.0f;
-	conf.n_epochs = conf.max_inc = 200;
+	sann_tconf_init(&conf, 0, 0);          // initialize training parameters
+	conf.vfrac = 0.0f;                     // no validation samples
+	conf.n_epochs = conf.max_inc = 200;    // always perform 200 iterations
 	sann_verbose = 1;
-	for (j = 0; j < n; ++j) {
-		int n_cor;
+	for (j = 0; j < n; ++j) {              // try different random seeds
 		sann_srand(j * 13 + 1);
-		m = sann_init_fnn(3, n_neurons);
+		m = sann_init_fnn(3, n_neurons);   // initialize the NN
 		sann_train(m, &conf, 4, x, y);
-		for (i = n_cor = 0; i < 4; ++i) {
+		for (i = 0; i < 4; ++i) {  // test if we can fit the XOR function
 			int s = (int)(x[i][0] + .5) ^ (int)(x[i][1] + .5);
 			sann_apply(m, x[i], &t, 0);
-			if (fabs(s - t) < 0.05) ++n_cor;
+			if (fabs(s - t) > 0.05) break; // confirm a fit if the output error is within 5%
 		}
-		if (n_cor == 4) ++n_succ;
-		sann_destroy(m);
+		if (i == 4) ++n_succ;
+		sann_destroy(m);                   // deallocate the model
 	}
 	printf("chance to fit the XOR function with %d hidden neurons: %.1f%%\n", n_neurons[1], 100. * n_succ / n);
 	return 0;
